04ce48f403f6c8e88ad509ae47be658850342cc0
Zavian Braithwaite <zavian.braithwaite@bridgewaterfinancegroup.co.uk>
2025-02-09 21:19:23 +0000

    display jokes on frontend

--- resources/js/components/JokesComponent.vue
+++ resources/js/components/JokesComponent.vue
@@ -1,14 +1,39 @@
 <template>
 <div>
-    <button class="btn btn-primary">
+    <button
+        @click="getJokes"
+        class="btn btn-primary">
         Generate Me Three Jokes
     </button>
+        <ul>
+            <li v-for="joke in jokes" :key="joke.id">{{ joke.setup }} : <b>{{ joke.punchline }}</b></li>
+        </ul>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'JokesComponent'
+    name: 'JokesComponent',
+    data() {
+        return {
+            jokes: {}
+        }
+    },
+
+    methods: {
+        //Get Jokes via the api route call
+        getJokes() {
+            axios.get('/api/jokes', {
+            })
+                .then((res) => {
+                    this.jokes = res.data;
+                    console.log(this.jokes);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
+    }
 }
 
 </script>
